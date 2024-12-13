e71db7d887fccde4b197237147806a66f3ccd7d1
Eduardo Rodrigues Rangel Malaquias <malaquias.eduardo18@gmail.com>
2024-12-13 12:10:57 -0300

    pagina do produto e loading

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Produtos />} />
         <Route path="/contato" element={<Contato />} />
-        <Route path="produto/:produto" element={<Produto />} />
+        <Route path="produto/:id" element={<Produto />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Produto/Produto.jsx
+++ src/Produto/Produto.jsx
@@ -1,9 +1,46 @@
-// import React from 'react'
+import React from 'react';
+import estilos from './Produto.module.css';
+import { useParams } from 'react-router-dom';
 
 const Produto = () => {
+  const [produto, setProduto] = React.useState(null);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    fetch(`https://ranekapi.origamid.dev/json/api/produto/${id}`)
+      .then((extrator) => extrator.json())
+      .then((dados) => setProduto(dados));
+  }, [id]);
+
+  if (!produto)
+    return (
+      <div className="container">
+        <div className={estilos.loading}></div>
+      </div>
+    );
   return (
-    <div>
-      <h1>Pagina de exibicao do produto</h1>
+    <div className={`container ${estilos.produto}`}>
+      <figure>
+        <img
+          src={produto.fotos[0].src}
+          alt={produto.fotos[0].title}
+          title={produto.fotos[0].title}
+        />
+      </figure>
+      <div className={estilos.informacoes}>
+        <h1>{produto.nome}</h1>
+        <span>{'R$ ' + produto.preco}</span>
+        <p>{produto.descricao}</p>
+      </div>
+      {produto.fotos.length > 1 && (
+        <figure>
+          <img
+            src={produto.fotos[1].src}
+            alt={produto.fotos[1].title}
+            title={produto.fotos[1].title}
+          />
+        </figure>
+      )}
     </div>
   );
 };
